388b0c298478c1c71ac7eeec47e35c8357f9f84d
Olexii <uuuunger@gmail.com>
2026-04-07 18:58:53 +0300

    Implement dequeue and peek methods in BiDirectionalPriorityQueue with mode handling

--- 2-Semester/Lab04/queue.js
+++ 2-Semester/Lab04/queue.js
@@ -1,3 +1,6 @@
+export { BiDirectionalPriorityQueue, Mode };
+
+
 const Mode = {
     HIGHEST: 'highest',
     LOWEST: 'lowest',
@@ -21,11 +24,89 @@ class BiDirectionalPriorityQueue {
         this.chronology = [];
         this.byPriority = [];
     }
-}
 
 enqueue(item, priority) {
     const node = new Node(item, priority);
     this.chronology.push(node);
     this.byPriority.push(node);
     this.byPriority.sort((a, b) => b.priority - a.priority);
+}
+
+dequeue(mode) {
+    switch (mode) {
+        case Mode.HIGHEST:
+            while (this.byPriority.length > 0 && this.byPriority[0].isDeleted) {
+                this.byPriority.shift();
+            }
+            if (this.byPriority.length === 0) return null;
+            const highestNode = this.byPriority.shift();
+            highestNode.isDeleted = true;
+            return highestNode.data;
+
+        case Mode.LOWEST:
+            while (this.byPriority.length > 0 && this.byPriority[this.byPriority.length - 1].isDeleted) {
+                this.byPriority.pop();
+            }
+            if (this.byPriority.length === 0) return null;
+            
+            const lowestNode = this.byPriority.pop();
+            lowestNode.isDeleted = true;
+            return lowestNode.data;
+
+        case Mode.OLDEST:
+            while (this.chronology.length > 0 && this.chronology[0].isDeleted) {
+                this.chronology.shift();
+            }
+            if (this.chronology.length === 0) return null;
+            
+            const oldestNode = this.chronology.shift();
+            oldestNode.isDeleted = true;
+            return oldestNode.data;
+
+        case Mode.NEWEST:
+            while (this.chronology.length > 0 && this.chronology[this.chronology.length - 1].isDeleted) {
+                this.chronology.pop();
+            }
+            if (this.chronology.length === 0) return null;
+            
+            const newestNode = this.chronology.pop();
+            newestNode.isDeleted = true;
+            return newestNode.data;
+
+        default:
+            throw new Error("Invalid mode");
+    }
+}
+
+peek(mode) {
+    switch (mode) {
+        case Mode.HIGHEST:
+            while (this.byPriority.length > 0 && this.byPriority[0].isDeleted) {
+                this.byPriority.shift();
+            }
+            return this.byPriority.length > 0 ? this.byPriority[0].data : null;
+
+        case Mode.LOWEST:
+            while (this.byPriority.length > 0 && this.byPriority[this.byPriority.length - 1].isDeleted) {
+                this.byPriority.pop();
+            }
+            return this.byPriority.length > 0 ? this.byPriority[this.byPriority.length - 1].data : null;
+
+        case Mode.OLDEST:
+            while (this.chronology.length > 0 && this.chronology[0].isDeleted) {
+                this.chronology.shift();
+            }
+            return this.chronology.length > 0 ? this.chronology[0].data : null;
+
+        case Mode.NEWEST:
+            while (this.chronology.length > 0 && this.chronology[this.chronology.length - 1].isDeleted) {
+                this.chronology.pop();
+            }
+            return this.chronology.length > 0 ? this.chronology[this.chronology.length - 1].data : null;
+
+        default:
+            throw new Error("Invalid mode");
+    }
+
+}
 }
